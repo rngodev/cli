3a96892bc6233e46cc27526fb343148d689afedb
aoifelee <153969545+aoifelee@users.noreply.github.com>
2025-06-16 23:02:19 -0400

    Accept API key in stdin, add logout (#2)

---
+++ src/logout.rs
@@ -0,0 +1,28 @@
+use crate::util::{Config, set_config};
+use anyhow::Result;
+use inquire::Confirm;
+
+pub async fn logout() -> Result<()> {
+    let config = crate::util::get_config()?;
+
+    if config.api_key.is_none() {
+        println!("You are not logged in");
+        return Ok(());
+    }
+
+    let confirmed = Confirm::new("Are you sure?").with_default(false).prompt()?;
+
+    if !confirmed {
+        println!("Log out cancelled");
+        return Ok(());
+    }
+
+    let new_config = Config {
+        api_key: None,
+        ..config
+    };
+
+    set_config(new_config)?;
+    println!("Successfully logged out");
+    Ok(())
+}
